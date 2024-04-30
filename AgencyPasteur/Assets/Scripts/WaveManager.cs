4ba05bd080085acd185f0b6ca6e41d056537c0c2
Emile Birraux <Emile.birraux@edu.devinci.fr>
2024-04-30 11:05:46 +0200

    [ADD] Test Wave management

---
+++ AgencyPasteur/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [SerializeField] Wave[] _waves;
+    [SerializeField] int _timeBtwWaves;
+    private Spawner[] spawners;
+    private int _waveNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawners = GetComponentsInChildren<Spawner>();
+        _waveNumber = 0;
+        StartCoroutine(WaveSystem());
+
+    }
+
+    // Update is called once per frame
+
+ IEnumerator WaveSystem()
+    {
+        Debug.Log(_waveNumber);
+        yield return new WaitForSeconds(2);
+        foreach (GameObject gameObject in _waves[_waveNumber].wave)
+        {
+            int count = UnityEngine.Random.Range(0,spawners.Length);
+            spawners[count].Spawn(gameObject);
+            yield return new WaitForSeconds(0.02f);
+        }
+        _waveNumber++;
+        yield return new WaitForSeconds(1);
+        if (_waveNumber < _waves.Length-1)
+        {
+            StartCoroutine(WaveSystem());
+        }
+    }
+}
+[Serializable]
+public class Wave
+{
+    [SerializeField] public GameObject[] wave;
+}
