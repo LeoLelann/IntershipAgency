6d805ceb4a336a41b9750c3a7007de29342d64a4
Emile Birraux <Emile.birraux@edu.devinci.fr>
2024-05-27 15:34:12 +0200

    [ADD] Pause Menu Basic, Interactable UI + Event

--- AgencyPasteur/Assets/Scripts/Player/Player.cs
+++ AgencyPasteur/Assets/Scripts/Player/Player.cs
@@ -22,6 +22,7 @@ public class Player : MonoBehaviour
     [SerializeField] private float _dashCD = 0.2f;
     [SerializeField] private float _dashDuration = 0.1f;
     [SerializeField] AnimationCurve _curve;
+    private Pause _pauseMenu; 
 
     private Rigidbody _rbOther;
     private Rigidbody _rb;
@@ -33,6 +34,7 @@ public class Player : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
         isInRange = false;
         _isDashing = false;
+        _pauseMenu = FindObjectOfType<Pause>();
     }
 
     void Update()
@@ -59,6 +61,10 @@ public class Player : MonoBehaviour
         _moveInput = context.ReadValue<Vector2>();
     }
 
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        _pauseMenu.SetPause();
+    }
 
     public void OnInteract(InputAction.CallbackContext context)
     {
