f46d3ba268c2c58e3800df245b838a021b36ed7d
Emile Birraux <Emile.birraux@edu.devinci.fr>
2024-06-17 19:49:27 +0200

    [FIX] Last one yeah Hopefully

--- AgencyPasteur/Assets/Scripts/UI Menu/SelectionCharacters/PlayerToken.cs
+++ AgencyPasteur/Assets/Scripts/UI Menu/SelectionCharacters/PlayerToken.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerToken : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
-
+    [SerializeField] private Sprite _spriteCadre;
+    [SerializeField] private Sprite _dftSpriteCadre;
     [SerializeField] UnityEvent _onChoosed;
     [SerializeField] UnityEvent _onChoosedCancelled;
 
     [SerializeField] TokenOnSelect _currentSelection;
+
 
     private Vector2 _moveInput;
     public bool _isChoosed { get; set; }
@@ -38,11 +41,18 @@ public class PlayerToken : MonoBehaviour
         if (_currentSelection == null) return;
 
         _onChoosed.Invoke();
+        TokenOnSelect tampon=_currentSelection;
+        _currentSelection.GetComponentInChildren<Image>().sprite = _spriteCadre;
+        _currentSelection.GetComponent<BoxCollider2D>().enabled = false;
+        _currentSelection = tampon;
         _isChoosed = true;
     }
     public void OnCancel(InputAction.CallbackContext ctx)
     {
         _onChoosedCancelled.Invoke();
+        Debug.Log(_currentSelection.name);
+        _currentSelection.GetComponentInChildren<Image>().sprite = _dftSpriteCadre;
+        _currentSelection.GetComponent<BoxCollider2D>().enabled =true;
         _isChoosed = false;
     }
 
