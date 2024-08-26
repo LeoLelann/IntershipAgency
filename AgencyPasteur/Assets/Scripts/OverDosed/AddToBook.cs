8b26d0f12c24dc71edd3661abaa102e9f2229073
Emile Birraux <Emile.birraux@edu.devinci.fr>
2024-08-26 21:45:59 +0200

    [ADD] dialogs in game

--- AgencyPasteur/Assets/Scripts/OverDosed/AddToBook.cs
+++ AgencyPasteur/Assets/Scripts/OverDosed/AddToBook.cs
@@ -24,6 +24,7 @@ public class AddToBook : MonoBehaviour
 
     IEnumerator MoveToward (Vector3 bookScreenPos)
     {
+        _newPage.Invoke();
         yield return new WaitForSeconds(2);
         float timer = 0;
         _book.LockedPage[GlasswareState].GetComponent<Page>().IsLocked = false;
